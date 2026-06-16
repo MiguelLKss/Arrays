35ca855859606719fb61ebe165730002541a1c30
MiguelLKss <miguellosskeller@gmail.com>
2026-06-16 10:32:46 -0300

    Count positive numbers from user input

--- 1.js
+++ 1.js
@@ -1,1 +1,12 @@
-console.log("Ola mundo!");
+let numeros = [];
+let positivos = 0;
+
+for (let i = 0; i < 6; i++) {
+    numeros[i] = Number(prompt("Digite um número:"));
+
+    if (numeros[i] > 0) {
+        positivos++;
+    }
+}
+
+console.log("Quantidade de positivos:", positivos);
